a9eac2ddd110e501bd8ae2af1f216c5c798927a3
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-28 18:21:38 +0200

    Create Login.jsx

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+function Login() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePassword = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  // @ts-ignore
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+  };
+
+  return (
+    <div
+      className="mx-auto flex flex-col justify-center space-y-6 w-xs sm:w-sm p-4
+     bg-white text-black/80 rounded-xl border border-gray-200 shadow-sm"
+    >
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold">Welcome back</h1>
+        <p className="text-sm text-gray-500">
+          Enter your email below to sign in to your account
+        </p>
+      </div>
+
+      <div className="grid gap-6">
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <label
+                className="text-sm font-medium leading-none"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-200 bg-background px-3 py-2 text-sm  
+                placeholder:text-gray-500 shadow-xs disabled:cursor-not-allowed disabled:opacity-50"
+                id="email"
+                placeholder="name@example.com"
+                required
+                type="email"
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <label
+                  className="text-sm font-medium leading-none"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  className="flex h-10 w-full rounded-md border border-gray-200 bg-background px-3 py-2 
+                  text-sm placeholder:text-gray-500 shadow-xs disabled:cursor-not-allowed disabled:opacity-50"
+                  id="password"
+                  required
+                  type={passwordVisible ? "text" : "password"}
+                />
+                <div
+                  className="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3 mb-5"
+                  onClick={togglePassword}
+                >
+                  {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+                </div>
+                <a
+                  href="#"
+                  className="underline-offset-3 text-sm underline font-semibold 
+                  text-violet-600/80 hover:text-violet-600/90 mt-1"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+            <button
+              className="cursor-pointer items-center justify-center rounded-md text-sm font-medium 
+             transition-colors duration-300 text-white/90 bg-violet-600/80 hover:bg-violet-600/85 
+             h-10 px-4 py-2 w-full"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        Don&apos;t have an account?{" "}
+        <a
+          href="#"
+          className="underline underline-offset-3 font-semibold text-violet-600/80 hover:text-violet-600/90"
+        >
+          Sign up
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
