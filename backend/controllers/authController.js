a693f8d7b8c1e99579b6befb64410add9db28216
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-01 00:09:25 +0200

    Update authController.js
    update login endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -47,19 +47,25 @@ export const login = asyncHandler(async (req, res) => {
   }
 
   const user = await User.findOne({ email });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    const token = generateToken(user._id);
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-
-    res.status(constants.OK).json({ message: "Login successful" });
-  } else {
+  if (!user || !(await bcrypt.compare(password, user.password))) {
     res.status(constants.UNAUTHORIZED);
     throw new Error("Invalid email or password");
   }
+
+  const token = generateToken(user._id);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(constants.OK).json({
+    message: "Login successful",
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
 });
