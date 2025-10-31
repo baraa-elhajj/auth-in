25c3278a9d21c2e89fa265c2dbb110e6c3dfe409
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-31 19:54:38 +0200

    Add react-toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AppContextProvider } from "@/contexts/AppContext.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <AppContextProvider>
     <StrictMode>
+      <ToastContainer />
       <App />
     </StrictMode>
   </AppContextProvider>
