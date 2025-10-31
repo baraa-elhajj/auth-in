2afa9dff69fd750d71e096ec961b4155f113b8da
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-01 01:45:01 +0200

    Update server.js
    use authRouter and errorHandler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongoDB.js";
+import authRouter from "./routes/authRoutes.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,5 +17,8 @@ const allowedOrigins = ["http://localhost:5173/"];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/auth", authRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
