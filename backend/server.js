cbdad69931b175df59ceac4bbc021beb1a0c2338
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-10-31 22:26:26 +0200

    Create mongoDB config

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongoDB.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
