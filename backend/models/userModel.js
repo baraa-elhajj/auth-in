cd018f5e418fcbeecf9a71356c6a0d1a9be4603f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-02 06:34:27 +0200

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,9 +4,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  verifyOtp: { type: String, default: "" },
-  verifyOtpExpireAt: { type: Number, default: 0 },
-  isVerified: { type: Boolean, default: false },
   resetOtp: { type: String, default: "" },
   resetOtpExpireAt: { type: Number, default: 0 },
 });
