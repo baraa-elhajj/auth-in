e73f84e1d27df058fb1b9a451273d20bce1751ca
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-02 15:47:07 +0200

    Create Spinner.jsx

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex space-x-1 items-center justify-center h-8">
+      <span className={`w-1 h-1 bg-white rounded-full animate-ping`}></span>
+      <span
+        className={`w-1 h-1 bg-white rounded-full animate-ping`}
+        style={{ animationDelay: "0.15s" }}
+      ></span>
+      <span
+        className={`w-1 h-1 bg-white rounded-full animate-ping`}
+        style={{ animationDelay: "0.3s" }}
+      ></span>
+    </div>
+  );
+};
+
+export default Spinner;
