f9c7f6c134a8ac5eb60bd5fe3d7ccd6184adeb91
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 16:12:43 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,59 @@
 import logo from "@/assets/images/authin_logo.png";
+import { AppContent } from "@/contexts/AppContext";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const { isLoggedIn, apiUrl, setIsLoggedIn, setUserData } =
+    useContext(AppContent);
+
+  const handleLogout = async () => {
+    setLoading(true);
+
+    await axios
+      .post(apiUrl + "/auth/logout")
+      .then((response) => {
+        setIsLoggedIn(false);
+        setUserData(null);
+        console.log(response);
+        toast.success(response.data.message ?? "Logged out successfully");
+      })
+      .catch((error) => {
+        toast.error(
+          error.response.data.message ??
+            "Something went wrong. Please try again later."
+        );
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="h-25 flex justify-between items-center bg-gray-50 mb-20">
-      <img className="w-27 h-27 ml-10" src={logo} />
-      <button
-        className="cursor-pointer py-2 px-4 mr-10 rounded-lg border border-black text-black 
-        hover:bg-violet-600/80 hover:border-transparent hover:text-white transition-colors duration-200"
-      >
-        Login
-      </button>
+      <img
+        onClick={() => navigate("/")}
+        className="cursor-pointer w-27 h-27 ml-10"
+        src={logo}
+      />
+      {isLoggedIn && (
+        <button
+          disabled={loading}
+          onClick={handleLogout}
+          className={`cursor-pointer py-2 px-4 mr-10 rounded-lg border border-black text-black 
+        hover:bg-violet-600/80 hover:border-transparent hover:text-white transition-colors duration-200 ${
+          loading ? "cursor-not-allowed opacity-80" : "cursor-pointer"
+        }`}
+        >
+          {loading ? <Spinner color="black" /> : "Logout"}
+        </button>
+      )}
     </div>
   );
 };
