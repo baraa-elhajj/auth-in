604b74af52dba0bfc1313850af79506cfa41af61
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 07:00:23 +0200

    Create home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import developer from "@/assets/images/developer.png";
+
+const Home = () => {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <div className="flex flex-col items-center gap-2">
+      <img className="w-40 h-40" src={developer} />
+      <h1 className="mt-5 text-xl font-semibold">Hello, Developer!</h1>
+      <h1 className="text-3xl">Welcome to AuthIn</h1>
+      <p className="text-lg w-md text-center">
+        AuthIn is a simple and cool fully functional Login component. Get
+        started below!
+      </p>
+      <button
+        className="mt-3 cursor-pointer py-2 px-5 rounded-3xl border border-black text-white bg-black 
+        hover:bg-violet-600/80 hover:border-transparent hover:text-white transition-colors duration-200"
+      >
+        Get Started
+      </button>
+    </div>
   );
-}
+};
+
+export default Home;
