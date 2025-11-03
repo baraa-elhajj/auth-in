3875f49858f2563f951337f8207282e8a3d700c9
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 16:15:06 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,9 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center gap-2">
       <img className="w-40 h-40" src={developer} />
-      <h1 className="mt-5 text-xl font-semibold">Hello, Developer!</h1>
+      <h1 className="mt-5 text-xl font-semibold">
+        Hello, {userData?.name ?? "Developer"}!
+      </h1>
       <h1 className="text-3xl">Welcome to AuthIn</h1>
       <p className="text-lg w-md text-center">
         AuthIn is a simple and cool fully functional Login component. Get
