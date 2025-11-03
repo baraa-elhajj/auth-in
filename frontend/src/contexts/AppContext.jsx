8d2e7a07476f415b97bf8dfcb77ed391b0c7197e
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 20:29:40 +0200

    Fix refresh page issue

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContent = createContext();
 
@@ -6,6 +7,26 @@ export const AppContextProvider = (props) => {
   const apiUrl = import.meta.env.VITE_API_URL;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      // Includes credentials like cookies, auth headers, etc.
+      axios.defaults.withCredentials = true;
+
+      await axios
+        .get(apiUrl + "/auth/user")
+        .then((response) => {
+          setIsLoggedIn(true);
+          setUserData(response.data);
+        })
+        .catch(() => {
+          setIsLoggedIn(false);
+          setUserData(null);
+        });
+    };
+
+    fetchUserData();
+  }, []);
 
   const value = {
     apiUrl,
