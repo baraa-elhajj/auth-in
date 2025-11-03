5957f63bca5aae6baf5cef552ab0a32b7ee16b25
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 16:12:31 +0200

    Update Spinner.jsx

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Spinner = () => {
+const Spinner = ({ color }) => {
   return (
     <div className="flex space-x-1 items-center justify-center h-8">
-      <span className={`w-1 h-1 bg-white rounded-full animate-ping`}></span>
+      <span className={`w-1 h-1 bg-${color} rounded-full animate-ping`}></span>
       <span
-        className={`w-1 h-1 bg-white rounded-full animate-ping`}
+        className={`w-1 h-1 bg-${color} rounded-full animate-ping`}
         style={{ animationDelay: "0.15s" }}
       ></span>
       <span
-        className={`w-1 h-1 bg-white rounded-full animate-ping`}
+        className={`w-1 h-1 bg-${color} rounded-full animate-ping`}
         style={{ animationDelay: "0.3s" }}
       ></span>
     </div>
