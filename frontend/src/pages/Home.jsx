b98b929a70a3b5f0ab3e6706c7d770c0436a13c7
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-03 16:12:57 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import developer from "@/assets/images/developer.png";
+import { AppContent } from "@/contexts/AppContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { isLoggedIn, userData } = useContext(AppContent);
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center gap-2">
       <img className="w-40 h-40" src={developer} />
@@ -10,12 +16,15 @@ const Home = () => {
         AuthIn is a simple and cool fully functional Login component. Get
         started below!
       </p>
-      <button
-        className="mt-3 cursor-pointer py-2 px-5 rounded-3xl border border-black text-white bg-black 
+      {!isLoggedIn && (
+        <button
+          onClick={() => navigate("/login")}
+          className="mt-3 cursor-pointer py-2 px-5 rounded-3xl border border-black text-white bg-black 
         hover:bg-violet-600/80 hover:border-transparent hover:text-white transition-colors duration-200"
-      >
-        Get Started
-      </button>
+        >
+          Get Started
+        </button>
+      )}
     </div>
   );
 };
