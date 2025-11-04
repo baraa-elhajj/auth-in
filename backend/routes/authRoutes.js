4326d5397b522141420dfd6b3f4b6bd227365ce6
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-04 23:20:28 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
   sendVerificationCode,
   resetPassword,
   getCurrentUser,
+  verifyCode,
 } from "../controllers/authController.js";
 import authTokenHandler from "../middlewares/authTokenHandler.js";
 
@@ -15,6 +16,7 @@ authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verification-code", sendVerificationCode);
+authRouter.put("/verify-code", verifyCode);
 authRouter.put("/reset-password", resetPassword);
 authRouter.get("/user", authTokenHandler, getCurrentUser);
 
