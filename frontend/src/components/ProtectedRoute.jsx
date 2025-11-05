d437f5829f3787d2e80988ce407293e434770e1a
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-05 10:09:52 +0200

    Delete ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({
-  element: Component,
-  isAuthenticated,
-}) {
-  return isAuthenticated ? Component : <Navigate to="/" replace />;
-}
