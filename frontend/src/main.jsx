dc54ba386f8845945db7e0199ab40b7880f0ab68
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-11-28 13:27:35 +0200

    Install and setup google auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AppContextProvider } from "@/contexts/AppContext.jsx";
 import { ToastContainer } from "react-toastify";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <AppContextProvider>
-    <StrictMode>
-      <ToastContainer />
-      <App />
-    </StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <StrictMode>
+        <ToastContainer />
+        <App />
+      </StrictMode>
+    </GoogleOAuthProvider>
   </AppContextProvider>
 );
